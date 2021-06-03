88ed21aca130b3f5e79e6036b3442f5898e985b9
Daniel <brikbusters@gmail.com>
2021-06-03 13:04:34 -0400

    bootdisk.py messages

--- bootdisk.py
+++ bootdisk.py
@@ -2,6 +2,17 @@
 import ptpy
 from ptpy import Canon
 
+# Commands to try:
+# EnableBootDisk
+# DisableBootDisk
+# TurnOffDisplay
+
 camera = ptpy.PTPy()
 with camera.session():
-    print(camera.eos_run_command("EnableBootDisk"))
+    print("Press ctrl+z if you get timeouts")
+    r = camera.eos_run_command("EnableBootDisk")
+    print(r)
+    if r.ResponseCode == "OK":
+        print("Command executed")
+    else:
+        print("Error, try again")
