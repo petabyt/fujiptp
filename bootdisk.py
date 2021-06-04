1bc20f9179b41dbc7823d326cbc635e7501aa745
Daniel <brikbusters@gmail.com>
2021-06-04 13:16:33 -0400

    Change script that works better (50% success rate?)

--- bootdisk.py
+++ bootdisk.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python
 import ptpy
 from ptpy import Canon
+from time import sleep
 
 # Commands to try:
 # EnableBootDisk
 # DisableBootDisk
 # TurnOffDisplay
 
+command = "EnableBootDisk"
+
 camera = ptpy.PTPy()
+
+print("Connected to the camera...")
+
 with camera.session():
-    print("Press ctrl+z if you get timeouts")
-    r = camera.eos_run_command("EnableBootDisk")
-    print(r)
-    if r.ResponseCode == "OK":
-        print("Command executed")
-    else:
-        print("Error, try again")
+    print("Waiting for a good time to send command...")
+    while True:
+        evt = camera.event()
+        if not evt:
+            result = camera.eos_run_command(command)
+            print(result)
+            if result.ResponseCode == "OK":
+                print("DryOS command run successfully, if no errors.")
+                break
+            else:
+                print("Bad ResponseCode. Will keep trying.")
+
