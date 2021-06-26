575475a8c8261620adb62ed5e70c18d498ade4e0
Daniel <brikbusters@gmail.com>
2021-06-26 13:40:09 -0400

    Add some info

--- bootflag.py
+++ bootflag.py
@@ -8,6 +8,10 @@ camera = ptpy.PTPy()
 print("Connected to the camera...")
 
 with camera.session():
+    info = camera.get_device_info()
+    print("Model: " + info.Model)
+    print("Version: " + info.DeviceVersion)
+    
     result = camera.eos_run_command("EnableBootDisk")
     if result.ResponseCode == 'OK':
         print("Boot flag enabled.")
